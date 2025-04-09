ab348837866e81399333042a90000b2b7cc2f6b6
Kenyi1515 <kenerc15@gmail.com>
2025-04-09 18:09:29 -0500

    Fix:Solucionando el error. KRC

--- src/app.py
+++ src/app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "Bye Code!"
+    return "Bye code"
+
+@app.route('/hello')
+def greeting():
+    return "Hello world"
 
 @app.route('/sum/<int:a>/<int:b>')
 def sum( a:int, b: int):
