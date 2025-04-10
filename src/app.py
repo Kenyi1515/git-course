3466dcba0758f40d73d6ed525bb610faa0e30217
Clone <kenerc15@gmail.com>
2025-04-09 20:56:37 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -15,8 +15,3 @@ def sum( a:int, b: int):
     nums_sum = a + b
     return f"La suma es: {str(nums_sum)}"
     
-@app.route('/multiply/<int:a>/<int:b>')
-def multiply( a:int, b: int):
-    result = float (a * b)
-    return f"El resultado es igual a: {str(result)}"
-
