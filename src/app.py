274b67d28804ef5e977674e1bf40eabf5d176a93
Kenyi1515 <kenerc15@gmail.com>
2025-04-09 19:07:58 -0500

    Corrigiendo

--- src/app.py
+++ src/app.py
@@ -14,9 +14,6 @@ def greeting():
 def sum( a:int, b: int):
     nums_sum = a + b
     return f"La suma es: {str(nums_sum)}"
-
-
-
     
 @app.route('/multiply/<int:a>/<int:b>')
 def multiply( a:int, b: int):
